4a1fcf5c9a1b26b8fd7ff2e5facdd96b9f32f9bd
Mick Cool <bmikdaam@gmail.com>
2022-03-09 23:51:20 +0100

    ajout de la fonction de rappel des events

--- src/events/onReady.ts
+++ src/events/onReady.ts
@@ -1,12 +1,14 @@
 import dotenv from 'dotenv';
+import { Client } from 'discord.js';
 import { REST } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v9';
 
 import { CommandList } from '../commands/_CommandList';
+import remainder from '../utils/remainder';
 
 dotenv.config();
 
-export const onReady = async () => {
+export const onReady = async (BOT: Client) => {
     const rest = new REST({version: '9'}).setToken(process.env.TOKEN as string);
 
     const commandData = CommandList.map(command => command.data.toJSON());
@@ -15,6 +17,9 @@ export const onReady = async () => {
         Routes.applicationGuildCommands(process.env.CLIENT_ID as string, process.env.GUILD_ID as string),
         {body: commandData}
     );
+    
     console.log('Commands successfully registered.');
     console.log('Bot is ready');
+
+    //remainder(BOT);
 }
